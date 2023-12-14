b39079b3f541ebb10b0cbe3a52d157455a8586f7
Komar7777 <152285576+Komar7777@users.noreply.github.com>
2023-12-14 14:23:31 +0300

    Final.pu
    Создание класса Square

--- Final.py
+++ Final.py
@@ -28,3 +28,34 @@ music = pygame.mixer.music.load('music/One-Love-Emotional-Piano-Strings(chosic.c
 pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=4096)
 pygame.mixer.music.set_volume(0.1)
 pygame.mixer.music.play(-1, 0.0)
+
+
+class Square:
+    def __init__(self, state=0):
+        self.state = state
+        self.number = 1
+
+        self.covered = True
+        self.flagged = False
+
+    def show_tiles(self, grid, x):
+        if self.flagged:
+            return
+
+        self.covered = False
+        neighbours = [i for i in
+                      [[x[0] - 1, x[1] - 1],
+                       [x[0], x[1] - 1],
+                       [x[0] + 1, x[1] - 1],
+                       [x[0] - 1, x[1]],
+                       [x[0] + 1, x[1]],
+                       [x[0] - 1, x[1] + 1],
+                       [x[0], x[1] + 1],
+                       [x[0] + 1, x[1] + 1]]
+                      if (0 <= i[0] < len(grid[0]) and 0 <= i[1] < len(grid))]
+
+        for n in neighbours:
+            if not grid[n[1]][n[0]].number and grid[n[1]][n[0]].covered:
+                grid[n[1]][n[0]].show_tiles(grid, n)
+            else:
+                grid[n[1]][n[0]].covered = False
